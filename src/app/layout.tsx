ab2131f068959f21bff3b3a44e9e6355c2b4ee99
shundahoy <shundahoy@gmail.com>
2023-08-12 14:31:00 +0900

    feat:レイアウト修正

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
 import "./globals.css";
 import type { Metadata } from "next";
 
@@ -17,7 +18,12 @@ export default function RootLayout({
     <html lang="ja">
       <body className="">
         <Header />
-        {children}
+        <div className="container mx-auto grid grid-cols-4 gap-8 pt-8">
+          <div className="col-span-3 px-4">{children}</div>
+          <div className="col-span-1 px-4">
+            <Sidebar />
+          </div>
+        </div>
         <Footer />
       </body>
     </html>
